137be0d0c221190a68cdc2a8078813e1f4469cb0
Shani yadav <91425321+Shaniyadaav@users.noreply.github.com>
2024-08-02 08:41:26 +0000

    conversionoperation

---
+++ .devcontainer/01_basics/03_conversuinOperation.js
@@ -0,0 +1,14 @@
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+//let isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
