583b6132a00dcf00c1b92ff76eaa514686fa7ef5
Shani yadav <91425321+Shaniyadaav@users.noreply.github.com>
2024-08-02 08:13:14 +0000

    datatype

---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,10 @@
+//"use strict";
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof "hitesh");
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
