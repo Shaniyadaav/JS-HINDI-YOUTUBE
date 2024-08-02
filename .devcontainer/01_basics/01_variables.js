799bea33496f9ae78da93427afa4e7548f6d12cc
Shani yadav <91425321+Shaniyadaav@users.noreply.github.com>
2024-08-02 07:44:41 +0000

    variable

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+accountEmail = "hc@12.com"
+accountPassword = 45123
+accountCity = "Bangaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
