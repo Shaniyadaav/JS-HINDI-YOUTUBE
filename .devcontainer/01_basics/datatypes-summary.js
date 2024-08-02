642d3088b6de4fc421ae0ae3f0fcafb49e1da2eb
Shani yadav <91425321+Shaniyadaav@users.noreply.github.com>
2024-08-02 11:48:43 +0000

    datatypes-summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsidetemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh"
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
