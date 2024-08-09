454f435e8f48bab3feec53107500e8084939ab86
Shani yadav <91425321+Shaniyadaav@users.noreply.github.com>
2024-08-09 13:20:01 +0000

    control flow

---
+++ .devcontainer/01_basics/15_control_flow.js
@@ -0,0 +1,49 @@
+const isUserloggedin = true;
+
+if (2==="2") {
+    //console.log("executed");
+}
+///*************************************** */
+
+
+const temperature = 41
+
+if (temperature < 50){
+   // console.log("less than 50");
+}else {
+    //console.log("temperature is greater than 50");
+}
+///********************************************** */
+
+const score = 200
+
+if(score>100){
+    let power = "fly"
+   // console.log('user power: ${power}');
+}else{
+   // console.log('user power: ${power}');
+}
+
+
+///************************************************ */
+///if else if else************************************
+const balance = 1000
+
+if(balance<500) {
+   // console.log("less than 500");
+}else if (balance<750){
+    //console.log("less than 750")
+}else if (balance<900){
+    //console.log("less than 900")
+}else {
+    //console.log ("less than 1200");
+}
+
+///***************************************** */
+
+const Userloggedin = true
+const debitCard = true
+
+if( Userloggedin && debitCard){
+    console.log("Allow to buy course")
+}
