589fefe1ece9841cdf29c6afa33f0c7ad077cdd2
Shani yadav <91425321+Shaniyadaav@users.noreply.github.com>
2024-08-09 13:21:16 +0000

    loopjs

---
+++ .devcontainer/01_basics/18_loops.js
@@ -0,0 +1,30 @@
+
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+       // console.log("5 is best number")
+    }
+    //console.log(element);
+    
+}
+
+
+
+for (let i = 1; i < 10; i++) {
+    //console.log('outer loop value: ${i}');
+    for (let j = 1; j < 10; j++) {
+
+     // console.log(i + '*' + j + '=' + j*j);
+        
+    }
+    
+}
+
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.lenght);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
