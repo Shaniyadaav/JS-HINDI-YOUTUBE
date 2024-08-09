b6e59756cf5534d175197f679a719ee76865e4b2
Shani yadav <91425321+Shaniyadaav@users.noreply.github.com>
2024-08-09 13:21:59 +0000

    breakandcontinue

---
+++ .devcontainer/01_basics/19_breakandcontinues.js
@@ -0,0 +1,16 @@
+//for (let index = 1; index <= 20; index++) {
+  //  if (index == 5){
+    //    console.log('Detected 5')
+      //  break
+   // }
+   // console.log(`value of i is ${index}`);
+//}
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5){
+        console.log('Detected 5')
+        continue
+    }
+    console.log(`value of i is ${index}`);
+}
+
